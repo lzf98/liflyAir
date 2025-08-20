427301c437d791d8df440cd7976b55fb17cf8850
lzf98 <2667798487@qq.com>
2025-08-20 23:51:27 +0800

    change

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import Script from 'next/script'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -67,6 +68,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={`${space_grotesk.variable} scroll-smooth`}
       suppressHydrationWarning
     >
+    <head>
+      {/* Google AdSense */}
+      <Script
+          id="adsense-script"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7311674354568913"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+      />
+    </head>
       <link
         rel="apple-touch-icon"
         sizes="76x76"
