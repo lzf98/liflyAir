ec12a06c03a1a79352404b74f00cd1a2b23d8bac
lzf98 <2667798487@qq.com>
2025-08-24 11:42:45 +0800

    change

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,13 +95,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
-      {/* ✅ 在这里加 Google AdSense 验证脚本 */}
+      {/*在这里加 Google AdSense 验证脚本*/}
       <Script
-          id="adsense-script"
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7311674354568913"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
+        id="adsense-script"
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7311674354568913"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
       />
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
