fee2cf5d15176fddcdd4318bd0737fa39783f724
lzf98 <2667798487@qq.com>
2025-08-31 00:43:11 +0800

    change asdasd

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
-import Script from 'next/script'  // 引入 Script
+import Script from 'next/script' // 引入 Script
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
